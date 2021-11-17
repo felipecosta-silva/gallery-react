97257119c0a93cc96869449b9831574e451543d5
Felipe Costa <felipecostati@gmail.com>
2021-11-17 16:54:47 -0300

    estruturando o app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as S from './App.styles';
+import * as PhotosService from './services/photos';
+
+const App = () => {
+  return (
+    <S.Container>
+      <S.Area>
+        <S.Header>Galeria de Fotos</S.Header>
+      </S.Area>
+    </S.Container>
+  )
+}
+
+export default App;
